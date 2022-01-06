8ded426181364128c529689963e384aa7266cf1a
SeungheonOh <dan.oh0721@gmail.com>
2022-01-05 20:09:12 -0600

    remove trash

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,49 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Main where
 
-import qualified MyLib (someFunc)
+import Data.APL
+rboard :: [[Int]]
+rboard = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+-- rboard = [
+--   [0, 0, 0, 0, 0, 0, 0],
+--   [0, 0, 0, 0, 0, 0, 0],
+--   [0, 0, 0, 0, 0, 0, 0],
+--   [0, 0, 0, 1, 0, 0, 0],
+--   [0, 0, 0, 0, 0, 0, 0],
+--   [0, 0, 0, 0, 0, 0, 0],
+--   [0, 0, 0, 0, 0, 0, 0]]
+
+board :: NestedArray Int
+board = reshape (fromList [length rboard, length $ head rboard]) $ fromList $ concat rboard
 
 main :: IO ()
 main = do
+  p $ life board
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+
+aAnd :: Int -> Int -> Int
+aAnd 1 1 = 1
+aAnd 1 0 = 0
+aAnd 0 1 = 0
+aAnd 0 0 = 0
+
+aOr :: Int -> Int -> Int
+aOr 1 1 = 1
+aOr 1 0 = 1
+aOr 0 1 = 1
+aOr 0 0 = 0
+
+aaor = op aOr
+aaand = op aAnd
+
+
+toInt = fmap fromEnum
+
+life = [apl|1,# ($aaor.$aaand) .$toInt 3,4 .eq .plus/ .plus/ 1,0,-1 (rotateFirst.) 1,0,-1 (rotate.) .enclose #|]
